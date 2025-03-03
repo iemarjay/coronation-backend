e75ccee2f38b9147e40b0f362523c8dea0684f4e
ekenefidel <echukwurah99@gmail.com>
2025-03-03 16:31:09 +0100

    fix: user service

--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -65,7 +65,7 @@ export class AuthGuard implements CanActivate {
       if (!userExists && payload.type === 'okta') {
         await this.userService.createSuperUser({
           email: payload.data.sub,
-          name: payload.data.name || payload.data.sub.split('@')[0],
+          name: payload.data.name || payload.data.sub,
           role: Role.staff,
           isOwner: false,
         });
